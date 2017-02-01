dfb2992d2e5cf14b3cfd05517e22c40406190887
un.def <un.def@ya.ru>
2017-02-02 00:20:35 +0300

    Fix dependencies in setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(name='BnW',
     author_email='blasux@blasux.ru',
     url='http://github.com/stiletto/bnw',
     packages=['bnw', 'bnw.core', 'bnw.formatting', 'bnw.handlers', 'bnw.scripts', 'bnw.search', 'bnw.web', 'bnw.xmpp'],
-    dependency_links=['http://github.com/mongodb/motor/tarball/master#egg=motor-0.1.2',
-                      'http://github.com/mongodb/mongo-python-driver/tarball/master#egg=pymongo-2.6',
-                      'http://github.com/stiletto/linkshit/tarball/master#egg=linkshit-0.2'],
+    dependency_links=['http://github.com/mongodb/motor/tarball/0.6.2#egg=motor-0.6.2',
+                      'http://github.com/mongodb/mongo-python-driver/tarball/master#egg=pymongo',
+                      'http://github.com/stiletto/linkshit/tarball/master#egg=linkshit'],
 
-    install_requires=['tornado>=2.0', 'twisted', 'Pillow', 'PyRSS2Gen', 'python-dateutil', 'misaka<2.0.0', 'motor', 'linkshit', 'libthumbor'],
+    install_requires=['tornado==4.3', 'twisted==16.2.0', 'Pillow', 'PyRSS2Gen', 'python-dateutil', 'misaka<2.0.0', 'motor', 'linkshit', 'libthumbor'],
     package_data={'bnw.web': ['templates/*.html','static/*.*', 'static/flot/*', 'static/web-socket-js/*']},
     entry_points = {
         'console_scripts': [
